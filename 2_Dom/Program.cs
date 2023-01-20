19b946b22b64e7906733f9a206985501e465dfc8
Катерина Андреевна <katrina_nova@mail.ru>
2023-01-20 21:23:13 +0500

    2 задача выполнена

--- 2_Dom/Program.cs
+++ 2_Dom/Program.cs
@@ -5,42 +5,42 @@
 
 while (true)
 {
-Console.WriteLine("Напиши первое целочисленное число:");
-int n1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Напиши второе целочисленное число:");
-int n2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Напиши первое целочисленное число:");
+    int n1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Напиши второе целочисленное число:");
+    int n2 = int.Parse(Console.ReadLine()!);
 
-if (n1==n2)
-{
-    Console.WriteLine("Введите неравные числа:");
-    continue;
-}
-    else
-    {
+    if (n1==n2)
+        {
+            Console.WriteLine("Введите неравные числа!");
+            continue;
+        }
+
     Console.WriteLine("Напиши третье целочисленное число:");
-    int n3 = int.Parse(Console.ReadLine());
-        if ((n3==n1)^(n3==n2));
+    int n3 = int.Parse(Console.ReadLine()!);
+    if ((n3==n1)^(n3==n2))
         {
-        Console.WriteLine("Введите неравные числа:");
-        continue;
+            Console.WriteLine("Введите неравные числа!");
+            continue;
         }
-    }
-        if ((n1>n2)^(n1>n3))
-        {
-            Console.WriteLine($"Максимальное число {n1}:");
+    if ((n1>n2)&&(n1>n3))
+       {
+         Console.WriteLine($"Максимальное число {n1}");
             break;
-        }
-            else
+       }
+        else
+        {
+            if (n2>n3)
             {
-                if (n2>n3)
-                {Console.WriteLine($"Максимальное число {n2}:");
+                Console.WriteLine($"Максимальное число {n2}");
                 break;
-                }
+            }
                 else 
                 {
-                    Console.WriteLine($"Максимальное число {n3}:");
+                    Console.WriteLine($"Максимальное число {n3}");
                     break;
                 }
+                break;
             }
 }
 
