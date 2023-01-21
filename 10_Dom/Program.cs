fdf7552e1687778575eb1ab12714754536c51994
Катерина Андреевна <katrina_nova@mail.ru>
2023-01-21 21:40:51 +0500

    3 задача 3 семинара

---
+++ 10_Dom/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine()!);
+int a = 1;
+while (a<= x)
+{
+    Console.Write($"{Math.Pow(a,3)}");
+    if (a!=x)
+    {
+        Console.Write(",");
+    }
+    a++;
+}
