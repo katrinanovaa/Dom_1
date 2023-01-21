bc4344277e94ca46c32a038a830c3bbfa7bd7483
Катерина Андреевна <katrina_nova@mail.ru>
2023-01-21 21:03:02 +0500

    1 задача 3 семинара

--- 8_Dom/Program.cs
+++ 8_Dom/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 19
+Напишите программу, которая принимает на вход 
+пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int n1 = int.Parse(Console.ReadLine()!);
+n1=Math.Abs(n1);
+int x1 = n1/10000; //Console.WriteLine($"Число {x1}");
+int x2 = n1/1000%10; //Console.WriteLine($"Число {x2}");
+int x4 = n1%100/10; //Console.WriteLine($"Число {x4}");
+int x5 = n1%10; //Console.WriteLine($"Число {x5}");
+if ((x1==x5)&&(x2==x4))
+{
+    Console.WriteLine($"Число {n1} палиндром");
+}
+else Console.WriteLine($"Число {n1} не палиндром");
