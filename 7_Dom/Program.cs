87d27d06dc82e20ba62b7de1d94d1c5e162246ea
Катерина Андреевна <katrina_nova@mail.ru>
2023-01-21 20:17:11 +0500

    3 задача 2 семинара выполнена

---
+++ 7_Dom/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.Clear();
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int n1 = int.Parse(Console.ReadLine()!);
+switch(n1)
+{
+    case 1: case 2: case 3: case 4: case 5: 
+    {
+        Console.WriteLine("Будний день"); 
+        break;
+    }
+    case 6: case 7: 
+    {
+        Console.WriteLine("Выходной день"); 
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Такого дня не существует, увы"); 
+        break;
+    }
+}
