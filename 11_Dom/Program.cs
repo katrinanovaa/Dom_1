3f7641f4662a016ad55d0d7e0aadc3a6dd67d105
Катерина Андреевна <katrina_nova@mail.ru>
2023-01-29 18:04:16 +0500

    1(25) задача 4 семинара

--- 11_Dom/Program.cs
+++ 11_Dom/Program.cs
@@ -3,34 +3,31 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16*/
 
-    Console.Clear();
-    Console.WriteLine("Введите число А");
-    int a = int.Parse(Console.ReadLine()!);
-    Console.WriteLine("Введите число B");
-    int b = int.Parse(Console.ReadLine()!);
-int Result(int result)
+double Result(double A, double B)
 {
-    //if (b<0) b=b*(-1);
-    int resultat =1;
-    for (int i=0; i <= b;)
+    double resultat =1;
+    if (B>=0)
     {
-        resultat*=a;
-        Console.WriteLine($"{resultat}");
-        i++;
+        for (int i=1; i <= B;)
+        {
+            resultat*=A;
+            i++;
+        }
+    }
+    else
+    {
+        B*=-1;
+        for (int i=1; i <= B;)
+        {
+            resultat*=1/A;
+            i++;
+        };
     }
     return (resultat);
 }
-Result(result);
-Console.WriteLine(Result(result));
-
-/*double raiseToPow(double x, int power)
-{
-    double result;
-    int i;
-    result =1.0;
-    for (i=1, i<=power;i++)
-    {
-        result = result*x;
-    }
-    return(result);
-}*/
+Console.Clear();
+Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Math.Round(Result(a, b), 6));
