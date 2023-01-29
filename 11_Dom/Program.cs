bd5684f4cad27e022125cad419967823c942fdea
Катерина Андреевна <katrina_nova@mail.ru>
2023-01-29 17:33:57 +0500

    2(27) задача 4 семинара

---
+++ 11_Dom/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Нельзя использовать библиотеку Math!
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+    Console.Clear();
+    Console.WriteLine("Введите число А");
+    int a = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите число B");
+    int b = int.Parse(Console.ReadLine()!);
+int Result(int result)
+{
+    //if (b<0) b=b*(-1);
+    int resultat =1;
+    for (int i=0; i <= b;)
+    {
+        resultat*=a;
+        Console.WriteLine($"{resultat}");
+        i++;
+    }
+    return (resultat);
+}
+Result(result);
+Console.WriteLine(Result(result));
+
+/*double raiseToPow(double x, int power)
+{
+    double result;
+    int i;
+    result =1.0;
+    for (i=1, i<=power;i++)
+    {
+        result = result*x;
+    }
+    return(result);
+}*/
---
+++ 12_Dom_27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+int GetSum(int number)
+{
+    int summa = 0;
+    int ostatok = 0;
+    while (number>0)
+    {
+        ostatok = number % 10;
+        number = number / 10;
+        summa += ostatok;
+    }
+    return summa;
+}
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine()!);
+if (numb<0) numb*=-1;
+
+
+Console.WriteLine(GetSum(numb));
