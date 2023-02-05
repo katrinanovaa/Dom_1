81319caca4d11e8fc0312837eb757dd82479b68b
Катерина Андреевна <katrina_nova@mail.ru>
2023-02-05 21:31:07 +0500

    1(47) задача 7 семинар

---
+++ 0_Chernovik/Program.cs
@@ -0,0 +1,9 @@
+﻿void main()
+{
+    Console.WriteLine("Введите число ");
+    int str = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(str);
+}
+
+
+main();
---
+++ 20_Dom_47/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m,n];
+    for (int i=0; i<m; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            result[i,j] = Math.Round(new Random().NextDouble(), 2)- new Random().Next(-100,100); 
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j=0; j< inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("введите количество строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(row,col);
+PrintArray(array);
