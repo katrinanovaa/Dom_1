5f118e279fc7e69bc799349e329830d4845bfa44
Катерина Андреевна <katrina_nova@mail.ru>
2023-02-19 20:55:03 +0500

    1(64) задача 9 семинар

---
+++ 28_Dom_64/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.Clear();
+Console.WriteLine("Программа, которая выведет все натуральные числа в промежутке от N до 1");
+Console.WriteLine("Введите N");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write($"N={number}-> ");
+PrintNumber(number);
+void PrintNumber(int i)
+{
+    if (i == 1)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+    Console.Write($"{i}, ");
+    i--;
+    PrintNumber(i);
+}
